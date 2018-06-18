ad722069b34e630599a220d7cb1931fe6686734a
Andrew Furth <andrew@versame.com>
2018-06-18 15:41:40 -0400

    printing command line args

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+console.log("\n\n@@@@ STARTING...");
+
+process.argv.forEach(function (val, index, array) {
+  console.log(index + ': ' + val);
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
