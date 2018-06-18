b1c51cd4d3417ad851f2aa48711103222ffba313
Andrew Furth <andrew@versame.com>
2018-06-18 16:59:32 -0400

    making n api requests; adding them all to a total bids array

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 let express = require('express');
 let app = express();
+let axios = require('axios');
 
-console.log("\n\n@@@@ STARTING...");
+const AUCTION_API_ROOT = "http://srx.pubcdn.net/auc.php"
 
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
-});
+// console.log("\n\n@@@@ STARTING...");
+
+// process.argv.forEach(function (val, index, array) {
+//   console.log(index + ': ' + val);
+// });
+
+let count = process.argv[2]
+let totalBids = [];
+
+for(let i = 1; i <= count; i++) {
+  console.log(`\n@@@ Request ${i}`)
+  requestBids()
+}
+
+
+function requestBids() {
+  let url = `${AUCTION_API_ROOT}?${String(Date.now())}`
+  console.log("@@@@ url", url);
+  
+  axios.get(url)
+    .then(response => {
+      let bids = response.data
+      console.log("\n\n\n Bids",bids.length)
+      totalBids = totalBids.concat(bids)
+      console.log("\n\n\n\n ******* totalBids", totalBids.length)
+    })
+    .catch(error => {
+      console.log(error);
+    }); 
+}
+
+
+
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
